b7784fe1d023a50c4ed37a27aff304e6af5197e0
barenike <m-s-ka@mail.ru>
2021-10-08 21:48:54 +0500

    Additional commit 3.0

--- src/main/java/com/reservation_system/host/entity/UserEntity.java
+++ src/main/java/com/reservation_system/host/entity/UserEntity.java
@@ -1,27 +1,33 @@
 package com.reservation_system.host.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class UserEntity {
+    // Не факт, что будет работать код с 12 строки по 17
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(generator = "uuid")
+    // name - название перемнной к которой мы хотим обратиться в БД?
+    // strategy - см. в Google
+    @GenericGenerator(name = "id", strategy = "uuid2")
+    private String uuid;
     private String login;
     private String password;
 
     public UserEntity() {
     }
 
-    public long getId() {
-        return id;
+    public String getUuid() {
+        return uuid;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 
     public String getLogin() {
@@ -38,5 +44,21 @@ public class UserEntity {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    //Пусть будет - пригодится
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        return Objects.equals(uuid, that.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid != null ? uuid.hashCode() : 0;
     }
 }
